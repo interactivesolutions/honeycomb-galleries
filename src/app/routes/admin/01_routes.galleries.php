04ee355884d74639070bd4b6fb317c284d445aff
Jevgenij Dmitrijev <jd@interactivesolutions.lt>
2017-06-14 20:48:23 +0300

    Added list routes

--- src/app/routes/admin/01_routes.galleries.php
+++ src/app/routes/admin/01_routes.galleries.php
@@ -10,6 +10,7 @@ Route::group(['prefix' => config('hc.admin_url'), 'middleware' => ['web', 'auth'
         Route::post('/', ['middleware' => ['acl:interactivesolutions_honeycomb_galleries_galleries_create'], 'uses' => 'HCGalleriesController@apiStore']);
         Route::delete('/', ['middleware' => ['acl:interactivesolutions_honeycomb_galleries_galleries_delete'], 'uses' => 'HCGalleriesController@apiDestroy']);
 
+        Route::get('list', ['as' => 'admin.api.galleries.list', 'middleware' => ['acl:interactivesolutions_honeycomb_galleries_galleries_list'], 'uses' => 'HCGalleriesController@apiIndex']);
         Route::post('restore', ['as' => 'admin.api.galleries.restore', 'middleware' => ['acl:interactivesolutions_honeycomb_galleries_galleries_update'], 'uses' => 'HCGalleriesController@apiRestore']);
         Route::post('merge', ['as' => 'admin.api.galleries.merge', 'middleware' => ['acl:interactivesolutions_honeycomb_galleries_galleries_create', 'acl:interactivesolutions_honeycomb_galleries_galleries_update'], 'uses' => 'HCGalleriesController@apiMerge']);
         Route::delete('force', ['as' => 'admin.api.galleries.force.multi', 'middleware' => ['acl:interactivesolutions_honeycomb_galleries_galleries_force_delete'], 'uses' => 'HCGalleriesController@apiForceDelete']);
